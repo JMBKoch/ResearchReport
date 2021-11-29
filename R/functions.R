5a76afe29da4d5e203b67861b8b4a047b62ab80a
JMBKoch <j.m.b.koch@protonmail.com>
2021-11-29 22:45:38 +0100

    first setup of pilot and also on the long term final simulation

---
+++ R/functions.R
@@ -0,0 +1,81 @@
+################################################################################
+# functions.R: All functions used in main.R, J.M.B. Koch, 2021
+################################################################################
+# simdat() ----------------------------------------------------------------
+# function to simulate data under desired model
+# function takes the four objects below as input; adjust as desired 
+# TBA: Extract the matrix from Hyper-parameters.R instead in final setup
+# 1. Lambda
+L1 <- matrix(c(.5,.75,.25), 3, 1)
+CL1 <- matrix(c(.25, 0, 0), 3, 1)
+L2 <- matrix(c(.1, .5, .25), 3 , 1)
+CL2 <- matrix(c(0, 0, .25), 3, 1)
+L <-  cbind(rbind(L1, CL1), rbind(CL2, L2))
+# 2. Psi
+Psi <- matrix(rep(NA, 4), ncol = 2)
+diag(Psi) <- 1
+Psi[1, 2] <- Psi[2, 1] <- 0.5
+# 3. Theta
+Theta <- diag(rep(0.3, 6))
+# 4. N
+N <- 200
+
+# function
+simdat <- function(L, Psi, Theta, N){
+      Sigma <- L%*%Psi%*%t(L) + Theta
+      Y <- mvtnorm::rmvnorm(N, rep(0, 6), Sigma)
+      return(Y)
+}
+
+
+# sampling() --------------------------------------------------------------
+# takes as output the chain-length, warmup, n_chains, n_parallel chains &
+#   all hyperparameters
+
+
+# output() ----------------------------------------------------------------
+# function takes rstan object and computes outcomes, and saves them
+
+output <- function(rstanObj, L, Psi, Theta){
+  
+  # estimates Lambda
+  main <- colMeans(as.matrix(FitA, pars = c("lambdaMainC[1]",
+                                    "lambdaMainC[2]",
+                                    "lambdaMainC[3]",
+                                    "lambdaMainC[4]",
+                                    "lambdaMainC[5]",
+                                    "lambdaMainC[6]")))
+  
+  cross <- colMeans(as.matrix(FitA, pars = c("lambdaCrossC[1]",
+                                             "lambdaCrossC[2]",
+                                             "lambdaCrossC[3]",
+                                             "lambdaCrossC[4]",
+                                             "lambdaCrossC[5]",
+                                             "lambdaCrossC[6]")))
+  # estimates Factor-Corr
+  corr <- colMeans(as.matrix(FitA, pars = c("Psi[2, 1]")))
+  # estimates Theta
+  theta <- colMeans(as.matrix(FitA, pars = "theta"))
+  
+  # Bias Lambda
+  biasMain <- abs(main-c(L1, L2))
+  biasCross <- abs(cross-c(CL1, CL2))
+  # Bias Factor Correlation
+  biasFactCorr <-  abs(corr-0.5)
+  # Bias Thetay
+  biasTheta <- abs(theta - rep(0.3, 6))
+  
+  # TBA: MSE
+  # TBA: True & False positives in estimating truly non-0 as non-0
+  
+  # TBA: save output in list
+  
+}
+
+
+# convergence() -----------------------------------------------------------
+# takes rstan object as input and computes and returns convergence diagnostics
+
+# main() ------------------------------------------------------------------
+# runs the main simulation, including the setup of parralell computing etc.
+
---
+++ stan/SmallVarNormal.stan
@@ -0,0 +1,99 @@
+data{
+  int<lower=0> N; // Sample Size
+  int<lower=1> P; // Number of Outcomes/ items
+  int<lower=1> Q; // Number of Factor
+  matrix[N, P] Y; // outcome matrix
+  real<lower=0> sigma; // hypterparameter prior cross-loadings
+}
+
+parameters{
+  vector<lower=0>[P] theta;
+  vector[P] lambdaMain;
+  vector[P] lambdaCross;
+  real factCor;
+}
+
+transformed parameters{
+  matrix[P, Q] LambdaUnc;
+  matrix[P, P] Theta;
+  //vector[Q] psi;
+  corr_matrix[Q] Psi;
+  // matrix[Q, Q] Psi;
+  matrix[P, P] Sigma;
+  vector[P] mu;
+
+  
+  Theta = diag_matrix(theta);
+  //psi = rep_vector(1, Q);
+  mu = rep_vector(0, P);
+  
+  // make loading matrix manually; todo: automate
+  LambdaUnc[1, 1] = lambdaMain[1];
+  LambdaUnc[2, 1] = lambdaMain[2];
+  LambdaUnc[3, 1] = lambdaMain[3];
+  LambdaUnc[4, 2] = lambdaMain[4];
+  LambdaUnc[5, 2] = lambdaMain[5];
+  LambdaUnc[6, 2] = lambdaMain[6];
+
+  LambdaUnc[4, 1] = lambdaCross[1];
+  LambdaUnc[5, 1] = lambdaCross[2];
+  LambdaUnc[6, 1] = lambdaCross[3];
+  LambdaUnc[1, 2] = lambdaCross[4];
+  LambdaUnc[2, 2] = lambdaCross[5];
+  LambdaUnc[3, 2] = lambdaCross[6];
+  
+  // make Psi manually; todo: automate
+  Psi[1, 1] = 1;
+  Psi[2, 2] = 1;
+  Psi[1, 2] = factCor;
+  Psi[2, 1] = factCor;
+
+  Sigma = LambdaUnc*Psi*LambdaUnc' + Theta; 
+}
+
+model{
+ //priors
+ lambdaMain ~ normal(0, 10);
+ lambdaCross ~ normal(0, sigma);
+ theta ~ cauchy(0, 5);
+  
+ //model
+ for(i in 1:N)
+   Y[i,] ~ multi_normal(mu, Sigma);
+}
+
+
+// sara's manier v. sign switchting correction: een maker voor en hoofd- en kruisladingen
+generated quantities{
+  
+  vector[P] lambdaMainC;
+  vector[P] lambdaCrossC;
+  corr_matrix[Q] PsiC; 
+  
+  PsiC = Psi;
+  lambdaMainC = lambdaMain;
+  lambdaCrossC = lambdaCross;
+
+// factor 1 sign switching correction [p = 1, marker item]  
+   if(lambdaMain[1] < 0){
+     lambdaMainC[1:3] = -1*lambdaMain[1:3];
+     lambdaCrossC[1:3] = -1*lambdaCross[1:3];
+
+    if(lambdaMainC[4] > 0){ 
+        PsiC[1, 2] = -1*Psi[1, 2];
+        PsiC[2, 1] = -1*Psi[1, 2];
+    }
+}
+
+// factor 2
+   if(lambdaMain[4] < 0){
+     lambdaMainC[4:6] = -1*lambdaMain[4:6];
+    lambdaCrossC[4:6] = -1*lambdaCross[4:6];
+     if(lambdaMain[1] > 0){
+       PsiC[1, 2] = -1*Psi[1, 2];
+       PsiC[2, 1] = -1*Psi[1, 2];
+    }
+  }
+ 
+}
+
