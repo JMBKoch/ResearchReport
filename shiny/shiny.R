4868d79b17a4521e0ce6d3dcd071d1c7ea7927f5
JMBKoch <j.m.b.koch@protonmail.com>
2021-12-17 12:40:47 +0100

    mostly deleted a bunch of unnecessary code

--- shiny/shiny.R
+++ shiny/shiny.R
@@ -1,21 +1,28 @@
 ###########################################################################
-# Know your Distributions
+# Shiny App for Visualizing Regularization Priors
 ###########################################################################
 
 # Libraries ---------------------------------------------------------------
 library(shiny) # shiny
+library(tidyverse)
+library(papaja)
+
+# Preperations ------------------------------------------------------------
+# 10000 draws
+ndraws <- 5e+05
+# sample Small Variance Normal Prior
+smallVar <- rnorm(ndraws, mean = 0, sd = 0.1)
 
 # UI ----------------------------------------------------------------------
 ui <- fluidPage(
   
   selectInput(inputId = "Distribution",
               label = "Choose a Distribution", 
-              choices = list(normal = "normal", 
-                             beta = "beta"
+              choices = list(SVNP = "normal", 
+                             RHP = "beta"
                              ,gamma = "gamma",
                              `inverse gamma` = "inverse gamma"
               )
-              
               
   ),
   
@@ -29,33 +36,7 @@ ui <- fluidPage(
                                 value = 1,
                                 label = ("Choose the Standard Deviation"))),
   
-  
-  conditionalPanel(condition = "input.Distribution == 'beta'",
-                   
-                   numericInput("shape1",
-                                value = 1,
-                                label = ("Choose the shape1 parameter")),
-                   numericInput("shape2",
-                                value = 1,
-                                label = ("Choose the shape2 parameter"))),
-  
-  conditionalPanel(condition = "input.Distribution == 'gamma'",
-                   
-                   numericInput("shape",
-                                value = 1,
-                                label = ("Choose the shape parameter")),
-                   numericInput("rate",
-                                value = 1,
-                                label = ("Choose the rate parameter"))),
-  
-  conditionalPanel(condition = "input.Distribution == 'inverse gamma'",
-                   
-                   numericInput("shape_inv",
-                                value = 1,
-                                label = ("Choose the shape parameter")),
-                   numericInput("rate_inv",
-                                value = 1,
-                                label = ("Choose the rate parameter"))),
+
   
   # output plot            
   plotOutput("plot")
@@ -70,32 +51,37 @@ server <- function(input, output) {
   # use renderPlot() function to make output$plot
   output$plot <- renderPlot({
     
-    # conditional on input
-    if (input$Distribution == "normal"){
-      
-      plot(density(rnorm(2000000, input$mean, input$sd)), main = "", xlab = "")
-      
-      
-    }else if(input$Distribution == "beta"){
-      
-      plot(density(rbeta(2000000, input$shape1, input$shape2)), main = "", xlab = "")
-      
-    }
-    
-    else if(input$Distribution == "gamma"){
-      
-      plot(density(rgamma(2000000, input$shape, input$rate)), main = "", xlab = "")
-      
-      
-    }else if(input$Distribution == "inverse gamma"){
-      
-      plot(density(invgamma::rinvgamma(2000000, input$shape_inv, input$rate_inv)),
-           main = "", xlab = "", form = 1, to = 2)
-      
-    }
+ 
     
   })
   
 }
 # Run App -----------------------------------------------------------------
 shinyApp(ui, server)
+
+
+
+
+
+
+
+# sample regularized horseshoe prior
+regHs <- rep(NA, ndraws)
+for(i in 1:ndraws){
+  c2 <- LaplacesDemon::rinvgamma(1, shape= nu/2, scale= nu*s2/2)
+  lambda <- t(1, scale=1)
+  tau <- LaplacesDemon::rhalfcauchy(1, scale=1)
+  lambda2_tilde <- c2 * lambda^2/(c2 + tau^2*lambda^2)
+  regHs[i] <- rnorm(1, 0, sqrt(tau^2*lambda2_tilde))
+}
+
+# make plot
+data.frame(dens = c(smallVar, regHs), 
+           prior = as.factor(rep(c("Small Variance Normal Prior (\u03c3\u00B2 = 0.01)", "Regularized Horseshoe Prior"), each = ndraws)),
+           asymp = rep(0, ndraws)) %>% 
+  ggplot(aes(x = dens, fill = prior, linetype = prior)) + 
+  geom_density(alpha = .5)+
+  geom_vline(aes(xintercept = asymp), linetype = "dashed") +
+  xlim(-.5, .5)+
+  labs(x = "Size Cross-Loading", title = NULL)+
+  theme_apa(legend.pos = "bottom")
